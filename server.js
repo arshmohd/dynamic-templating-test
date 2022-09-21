caa5218bcd2f369a3adbf470f0097b1836522095
Mohammed Irfan <irfan.arshad4@gmail.com>
2022-09-21 16:57:17 +0530

    This is was in-class activity on dynamic-templating

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
+const {students} = require('./exam-info')
+
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 const app = express();
 
@@ -12,10 +16,23 @@ app.use(express.static('public'));
 
 // 1: in the home,list all the students who took the exam (list all the students)
 
-// ... Your code here
+app.get('/', (req, res) => {
+  res.render('full-list.hbs', {students})
+})
+
+
 
 // 2: in the '/results' list all the students who passed the test and their score.
 // Also, students should be in descending order based on their score.
+
+app.get('/results', (req, res) => {
+  let filterStudents = students.filter((students)=>{
+    return students.hasPassed;
+  })
+ 
+  filterStudents.sort((a, b) => a.score - b.score )
+  res.render('results.hbs', {students: filterStudents})
+})
 
 // ... Your code here
 
